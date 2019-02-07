028838bb56c509744ab13452031bc9f3eaa7d9fb
ajeetrai707 <ajeetrai707@gmail.com>
2019-02-07 16:26:07 +0530

    lines_added to cleanup script
    Signed-off-by: ajeetrai707 <ajeetrai707@gmail.com>

---
+++ docker_cleanup_script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+
+set -x
+ sh  docker rm $(docker ps -a -q) 
+ sh  docker rmi --unused      // remove all images that are not referenced by active containers
+
+
+lines=$(sudo docker images -q|wc -l) 
+echo $lines
+lines1=$(($lines-1))
+sudo docker rmi   $(docker images -q  | tail -n-$lines1)
+
+
+
